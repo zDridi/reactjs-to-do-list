648eee3c9e0ca4796e9c9e8b8c6f1c6ee033a016
zDridi <dridi.zied@courrier.uqam.ca>
2021-11-16 22:27:38 -0500

    Déplacement de l'élément HTML <h1> dans ToDoList.js et utilisation du tag <React.Fragment> pour incorporer deux éléments HTML de même niveau dans un composant React

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends React.Component{
     render(){
         return(
             <section id="todo">
-                <h1 classNameName="m-3">Liste de tâches</h1>
+                
                 <ToDoList />
                 <NavBar />
 
