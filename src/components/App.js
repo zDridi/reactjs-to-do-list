bf7698fd4bf3487ac11f4fdc04127c50968a3ae8
zDridi <dridi.zied@courrier.uqam.ca>
2021-11-19 15:55:23 -0500

    Incorporer react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from 'react'
+// import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { BrowserRouter, Routes ,Route } from 'react-router-dom';
 import ToDoList from './ToDoList';
 import NavBar from './NavBar';
+import AddTask from './AddTask'
+
 
 class App extends React.Component{
     render(){
         return(
             <section id="todo">
-                
-                <ToDoList />
-                <NavBar />
-
+                <BrowserRouter>
+                    <Routes>
+                        {/* <Route path="/add-task" component={AddTask} /> */}
+                        <Route path='/add-task' element={<AddTask/>} />
+                        {/* <Route path="/" component={ToDoList} /> */}
+                        <Route path='/' element={<ToDoList/>} />
+                        
+                    </Routes>
+                    <NavBar />
+                </BrowserRouter>
             </section>
         )
     }
